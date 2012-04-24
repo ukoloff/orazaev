5d6a98ff8b8eac9b77bcd04eaeec295e4c3b7f5f
Aman Orazaev <AOrazaev@gmail.com>
2012-04-24 20:19:39 +0400

    TSocket class added, need testingi.

--- socket/TSocket.h
+++ socket/TSocket.h
@@ -5,6 +5,8 @@
  *
  *	gmail me: AOrazaev[at]gmail.com
  */
+#ifndef TSOCKET_H
+#define TSOCKET_H
 #include <string>
 #include <sys/socket.h>
 #include <sys/types.h> 
@@ -12,24 +14,31 @@
 #include <netdb.h>
 
 class TSocket {
-	TSocket(const TSocket& copy);
-    TSocket& operator= (TSocket&);
-    TSocket();
-
     int sockfd;
-    struct sockaddr_in serv_addr;
-    struct sockaddr_in cli_addr;
-public:
-	TSocket(std::string hostname, int portno);
-    virtual ~TSocket();
+    struct sockaddr_in addr;
 
-    void write(std::string msg);
-    std::string read();
-    
+    TSocket() {}
+public:
+	TSocket(const std::string& hostname, const int& portno);
+    TSocket(const int& Sockfd, const struct sockaddr_in& Addr);
+    TSocket(const int& portno);
+
+    virtual ~TSocket(); 
+
+    void Connect();
+    void Listen();
+    void Bind();
+    TSocket Accept();
+
+    void Write(std::string msg);
+    std::string Read();
+
     //exceptions
     class ESocket {};
     class EBind    : public ESocket {};
     class EConnect : public ESocket {};
+    class EListen  : public ESocket {};
+    class EAccept  : public ESocket {};
     class ERead    : public ESocket {};
     class EWrite   : public ESocket {};
 
@@ -37,4 +46,4 @@ public:
     static const int CBUF_SIZE = 256;
 };
 
-
+#endif
