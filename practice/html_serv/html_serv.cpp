051fbe3e94ecec0a3873b49862df1338a447673e
Aman Orazaev <AOrazaev@gmail.com>
2012-05-03 14:39:26 +0400

    http header fix.

--- practice/html_serv/html_serv.cpp
+++ practice/html_serv/html_serv.cpp
@@ -23,6 +23,7 @@ void start_server(TSocket& connection) {
         buf += std::string(c) + "\n";
     }
 
+/*
     sprintf(c, "HTTP/1.1 200 OK\n\
 Server: nginx\n\
 Date: Thu, 25 Feb 2012 12:31:25 GMT\n\
@@ -31,25 +32,22 @@ Content-Type: text/html; charset=utf-8\n\
 Content-Length: %d\n\
 Transfer-Encoding: chunked\n\
 Connection: keep-alive\n\n", buf.size());
-    
+*/
+
+    sprintf(c, "HTTP/1.1 200 OK\n\
+Content-Type: text/html; charset=utf-8\n\
+Content-Length: %d\n\n", buf.size());
+/*
+    sprintf(c, "HTTP/1.1 200 OK\n\
+Content-Type: text/html; charset=utf-8\n\
+\n");
+*/
     std::string head = std::string(c);
 
     buf = head + buf + "\n";
 
     std::cout << "Sending: " << std::endl << buf << std::endl;
     connection.Write(buf, buf.size());
-    //std::cout << "Sending:" << std::endl << head << std::endl;
-
-    //connection.Write(head, head.size() - 1);
-
-    //std::cout << "Head send ok" << std::endl;
-
-    //connection.Write(buf, buf.size());
-
-    //for(int x = 0; x < buf.size(); x++) {
-    //    char z[2] = { buf[x], 0 };
-    //    connection.Write(std::string(z), 1);
-    //}
 }
 
 int main(int argc, char** argv) {
