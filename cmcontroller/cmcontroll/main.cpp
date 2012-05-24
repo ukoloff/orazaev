ad9b99b707f653c9aae587413973a7b4889d4da8
Aman Orazaev <AOrazaev@gmail.com>
2012-05-24 19:24:58 +0400

    Changes in cmcontroller: *) more help *) just server

--- cmcontroller/cmcontroll/main.cpp
+++ cmcontroller/cmcontroll/main.cpp
@@ -6,32 +6,60 @@
 #include<stdlib.h>
 #include<getopt.h>
 
-static float version = 0.35;
+static float version = 0.4;
 
 static std::string USAGE = "\
 cmcontroll --- client-server application for cluster master tests.\n\
 \n\
 Usage:\n\
-    cmcontroll --port NUM                                   : start cmcontroll server on port NUM.\n\
-    cmcontroll --host NAME --port NUM [--request REQUEST]   : send REQUEST to server on host NAME and port NUM.\n\
+    cmcontroll --port NUM [-l LOG] [-m MSG] [-P PID]            : start cmcontroll server on port NUM.\n\
+    cmcontroll -v                                               : get version of program. \n\
+    cmcontroll -?                                               : get help message. \n\
 \n\
 Options:\n\
     -p --port     : set port.\n\
-    -s --host     : set host.\n\
-    -r --request  : set request to server.\n\
     -l --main-log : set main log. \n\
     -m --msg-log  : set message log. \n\
+    -P --pid-file : set pid file. \n\
     -v --version  : print version.\n\
-    -h --help     : print this message.";
+    -? --help     : print this message.\n\
+\n\
+--------------------------------------------------------------------------------------------------------\n\
+Requests syntax.(Warning: All requests without quetes!)\n\
+    Addition: \n\
+    '+ HOST: STARGET'                                           : +1 for non cluster target STARGET on host HOST.\n\
+                                                                  It add in data next sentances:\n\
+                                                                  *) 'HOST: STARGET'       --- all request.\n\
+                                                                  *) 'STARGET'             --- only target name. \n\
+    '+ HOST: TARGET.CLUSTER'                                    : +1 for target TARGET with cluster CLUSTER on host HOST.\n\
+                                                                  It add in data next sentances:\n\
+                                                                  *) 'HOST: TARGET.CLUSTER' --- all request.\n\
+                                                                  *) 'TARGET.'              --- target name(with period).\n\
+                                                                  *) 'TARGET.CLUSTER'       --- target name with cluster.\n\
+                                                                  *) 'HOST: TARGET.'        --- host with target name.\n\
+    \n\
+    Request count of complete targets: \n\
+    '? HOST: NAME'                                              : If non cluster target NAME is complete in host HOST,\n\
+                                                                  that request return 1, becouse count of complete\n\
+                                                                  targets by this request is 1.(If hard, see addition).\n\
+    '? HOST: NAME.'                                             : Get count of complete cluster targets NAME on host HOST.'\n\
+    '? HOST: NAME.CLUSTER'                                      : Is cluster CLUSTER of target NAME complete on host HOST?\n\
+                                                                  If it so, request get 1.\n\
+    '? NAME'                                                    : Get count of non cluster target NAME execution on \n\
+                                                                  all hosts.\n\
+    '? NAME.'                                                   : Get count of all clusters of target NAME execution on\n\
+                                                                  all hosts.\n\
+    '? NAME.CLUSTER'                                            : Get count of all executions of NAME.CLUSTER on\n\
+                                                                  all hosts.\n\
+    \n\
+    Dump data:\n\
+    'd FNAME'                                                   : Dump server information about executed targets\n\
+                                                                  to file FILENAME.\n\
+    \n\
+    If it's hard to understand what it is --- just ask me. (orazaev@)";
 
 
 
-void cmc_client_start(const std::string& host, const int& port, const std::string& request) {
-    //connect to server
-    //write request
-    //get ans
-}
-    
 void cmc_server_start(const int& port, const std::string& pidfilename, const std::string& mainLog, const std::string& msgLog) {
     int pid = fork();
 
@@ -39,7 +67,7 @@ void cmc_server_start(const int& port, const std::string& pidfilename, const std
         std::cerr << "Error: can't fork server process." << std::endl;
         exit(2);
     }
-    else if (pid == 0) {
+    else if (pid != 0) {
         // main process save pid
         if (pidfilename == "") return;
 
@@ -61,26 +89,20 @@ void cmc_server_start(const int& port, const std::string& pidfilename, const std
         if (!msgLog.empty())  serv.setMsgLog(msgLog);
         
         serv.startListen();
-
-        //while(true) {}
     }
 }
 
 void cmc_start(int argc, char** argv) {
     int opt;
-    std::string host = "";
     int port = 0;
-    std::string request = "";
     std::string pidfile = "";
     std::string mainLog = "";
     std::string msgLog  = "";
     
     while (1) {
         static struct option long_opts[] = {
-            {"help",     no_argument,          0, 'h'},
+            {"help",     no_argument,          0, '?'},
             {"port",     required_argument,    0, 'p'},
-            {"host",     required_argument,    0, 's'},
-            {"request",  required_argument,    0, 'r'},
             {"version",  no_argument,          0, 'v'},
             {"pid",      required_argument,    0, 'P'},
             {"msg-log",  required_argument,    0, 'm'},
@@ -90,19 +112,17 @@ void cmc_start(int argc, char** argv) {
 
         int opt_index = 0;
     
-        opt = getopt_long(argc, argv, "hvp:s:r:P:l:m:", long_opts, &opt_index);
+        opt = getopt_long(argc, argv, "?vp:P:l:m:", long_opts, &opt_index);
 
         if (opt == -1)
             break;
 
         switch(opt) {
             case 'p': port = atoi(optarg); break;
-            case 's': host    = std::string(optarg); break;
-            case 'r': request = std::string(optarg); break;
             case 'P': pidfile = std::string(optarg); break;
             case 'm': msgLog  = std::string(optarg); break;
             case 'l': mainLog = std::string(optarg); break;
-            case 'h': 
+            case '?': 
                 std::cout << USAGE << std::endl; 
                 exit(0); 
                 break;
@@ -118,22 +138,11 @@ void cmc_start(int argc, char** argv) {
     }
 
     if (port == 0) {
-        std::cout << "Error: you must set port number to start server or host and port to start client" << std::endl;
+        std::cout << "Error: you must set port number to start server" << std::endl;
         exit(1);
     }
 
-    if (request != "" && host == "") {
-        std::cout << "Error: server haven't request option" << std::endl;
-    }
-
-    if (host != "") {
-        //client
-        cmc_client_start(host, port, request);
-    }
-    else {
-        //server
-        cmc_server_start(port, pidfile, mainLog, msgLog);
-    }
+    cmc_server_start(port, pidfile, mainLog, msgLog);
 }
 
 int main(int argc, char** argv) {
