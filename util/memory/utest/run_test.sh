f6cd32bd9439acc7267bee3bab645726583b3073
Aman Orazaev <AOrazaev@gmail.com>
2012-06-08 16:16:36 +0400

    new utest-case and small refactoring of ut_tptr

--- util/memory/utest/run_test.sh
+++ util/memory/utest/run_test.sh
@@ -3,6 +3,6 @@
 make clean > /dev/null 2>&1
 make > test_build.log 2>&1 || exit 1
 
-./utest
+valgrind ./utest
 
 rm test_build.log
--- util/memory/utest/ut_tptr.cpp
+++ util/memory/utest/ut_tptr.cpp
@@ -2,28 +2,45 @@
 #include "../tptr.h"
 #include <string>
 #include <iostream>
+#include <vector>
+
+void func(TPtr<std::string> pts) {
+    *pts = "Func changes.";
+}
+
+std::string addSpaces(std::string s, int x = 50) {
+    if (s.size() < x )
+        s.resize(x, '.');
+    return s;
+}
 
 
 int main() {
-    std::cout << "CONSTRUCTION WITHOUT ARGS";
+    std::cout << addSpaces("CONSTRUCTION WITHOUT ARGS");
     TPtr<std::string> a;
     TPtr<std::string> b;
     TPtr<std::string> c;
     std::cout << " [ PASS ]" << std::endl;
 
-    std::cout << "CONSTRUCTION WITH ARGS";
+
+
+    std::cout << addSpaces("CONSTRUCTION WITH ARGS");
     TPtr<std::string> d(new std::string("Hello, memory!"));
     TPtr<int> e(new int(777));
     std::cout << " [ PASS ]" << std::endl;
 
-    std::cout << "REFCOUNT TEST";
+
+
+    std::cout << addSpaces("REFCOUNT TEST");
     c = d;
     if (a.getRefCount() != 0 || b.getRefCount() != 0 || c.getRefCount() != 2 || d.getRefCount() != 2 || e.getRefCount() != 1)
         std::cout << " [ FAIL ]" << std::endl;
     else
         std::cout << " [ PASS ]" << std::endl;
     
-    std::cout << "DESTRUCTION AND COPY CONSTRUCTION";
+
+
+    std::cout << addSpaces("DESTRUCTION AND COPY CONSTRUCTION");
     c = TPtr<std::string>();
     {
         TPtr<std::string> tempd = d;
@@ -33,17 +50,33 @@ int main() {
     else
         std::cout << " [ PASS ]" << std::endl;
 
-    std::cout << "OPERATIONS";
+
+
+    std::cout << addSpaces("OPERATIONS");
     *e -= 444;
     *d = std::string("Hello, memory! I'm here!");
     a = b;
     c = b;
     a = c;
     b = TPtr<std::string>(new std::string("Dolly"));
-    if (*e != 333 || *d != "Hello, memory! I'm here!" || a.getRefCount() != 0 || b.getRefCount() != 1 || c.getRefCount() != 0 || d.getRefCount() != 1)
+    if (*e != 333 || *d != "Hello, memory! I'm here!" || a.getRefCount() != 0 || b.getRefCount() != 1 || c.getRefCount() != 0 || d.getRefCount() != 1 || *(d.get()) != "Hello, memory! I'm here!")
         std::cout << " [ FAIL ]" << std::endl;
     else
         std::cout << " [ PASS ]" << std::endl;
+
+
+
+    std::cout << addSpaces("AS FUNCTION ARG");
+    std::vector<TPtr<int> > pvector;
+    pvector.push_back(e);
+    pvector.push_back(e);
+    pvector.erase(pvector.begin() + 1);
+    func(d);
+    if (e.getRefCount() != 2 || *pvector[0] != 333 || pvector.size() != 1 || d.getRefCount() != 1 || *d != "Func changes.")
+        std::cout << " [ FAIL ]" << std::endl;
+    else
+        std::cout << " [ PASS ]" << std::endl;
+    
     
     return 0;
 }
