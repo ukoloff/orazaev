21a2595085d9cb2a5b4d0f032d453094ee344c44
Aman Orazaev <aorazaev@gmail.com>
2012-09-29 15:53:13 +0400

    Refactoring
    Move test functions to separete file.
    Move rotate to separete header.

---
+++ shad/cxx/dz1/aorazaev_z3.h
@@ -0,0 +1,41 @@
+/*
+ *      C++ homework. Problem 3.
+ *      
+ *      Due date: 04.09.12
+ *
+ *      Author: Aman Orazaev
+ */
+#ifndef AROAZAEV_DZ1_Z3_H
+#define AORAZAEV_DZ1_Z3_H
+
+#include<algorithm>
+
+
+
+template <typename T>
+void shift(T begin, T end) {
+    if (begin == end || end - begin == 1)
+        return;
+
+    std::swap(*begin++, *--end);
+
+    for (T it = (end); it != begin; --it) {
+        std::swap(*it, *(it - 1));
+    }
+}
+
+template <typename T>
+void rotate(T begin, T end, int k) {
+    if (begin == end)
+        return;
+    
+    while (k < 0)
+        k += end - begin;
+
+    while(k-- > 0)
+        shift(begin, end);
+}
+
+
+
+#endif /* AROAZAEV_DZ1_Z3_H */
