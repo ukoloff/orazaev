657332182f543d1d10d7417a8c9bd0cc136eec83
Aman Orazaev <AOrazaev@gmail.com>
2012-10-11 15:54:45 +0400

    edu/shad/babenko: memory optimization -trailspaces

--- edu/shad/babenko/code/dz2_z1.cpp
+++ edu/shad/babenko/code/dz2_z1.cpp
@@ -2,6 +2,7 @@
 #include<vector>
 #include<stdexcept>
 #include<time.h>
+#include<cstdlib>
 
 template <typename A>
 void printElements(A begin, A end, std::string space = " ");
@@ -16,23 +17,26 @@ size_t sizeOfLCS(A begin0, A end0, B begin1, B end1) {
 
     size_t seqSize0 = end0 - begin0;
     size_t seqSize1 = end1 - begin1;
-    
-    std::vector<std::vector<size_t> > L = 
-        std::vector<std::vector<size_t> >(
-            seqSize0 + 1, std::vector<size_t>(seqSize1 + 1, 0));
-    
+
+    std::vector<size_t> newL[2] = {
+        std::vector<size_t>(seqSize1 + 1, 0),
+        std::vector<size_t>(seqSize1 + 1, 0)
+    };
+    int line = 0;
+
     for(size_t i0 = 0; i0 < seqSize0; ++i0) {
         for (size_t i1 = 0; i1 < seqSize1; ++i1) {
             if (*(begin0 + i0) == *(begin1 + i1)) {
-                L[i0 + 1][i1 + 1] = L[i0][i1] + 1;
+                newL[line][i1 + 1] = newL[(line + 1) % 2][i1] + 1;
             } else {
-                L[i0 + 1][i1 + 1] =
-                    max(L[i0 + 1][i1], L[i0][i1 + 1]);
+                newL[line][i1 + 1] =
+                    max(newL[(line + 1) % 2][i1 + 1], newL[line][i1]);
             }
         }
+        line = (line + 1) % 2;
     }
-    
-    return L[seqSize0][seqSize1];
+
+    return newL[(line + 1) % 2][seqSize1];
 }
 
 
@@ -51,7 +55,7 @@ void printElements(A begin, A end, std::string space = " ") {
 
 template <typename A, typename B>
 double sizeOfLcsIs(A begin0, A end0, B begin1, B end1, size_t size) {
-    
+
     clock_t start = clock();
     size_t result = sizeOfLCS(begin0, end0, begin1, end1);
     clock_t end = clock();
@@ -62,10 +66,10 @@ double sizeOfLcsIs(A begin0, A end0, B begin1, B end1, size_t size) {
         printElements(begin0, end0);
         std::cout << "Sequence1: ";
         printElements(begin1, end1);
-        
-        std:: cout << "Result of sizeOfLCS() = " 
+
+        std:: cout << "Result of sizeOfLCS() = "
             << result << std::endl;
-        std:: cout << "Expected result       = " 
+        std:: cout << "Expected result       = "
             << size << std::endl;
         throw std::runtime_error("TEST FAILED!");
     }
@@ -79,7 +83,7 @@ void simpleTest() {
     int seq1[5] = {1, 3, 5, 7, 9};
 
     std::cout << "Simple test... ";
-    double time = 
+    double time =
         sizeOfLcsIs(seq0, seq0 + 5, seq1, seq1 + 5, 3);
     std::cout << " " << time << " secondes" << std::endl;
 }
@@ -87,9 +91,9 @@ void simpleTest() {
 void emptyTest() {
     std::vector<int> seq0;
     std::vector<int> seq1;
-    
+
     std::cout << "Empty test...";
-    double time = 
+    double time =
         sizeOfLcsIs(seq0.begin(), seq0.end(), seq1.begin(), seq1.end(), 0);
     std::cout << " " << time << " secondes" << std::endl;
 }
@@ -99,7 +103,7 @@ void trivialTest(int a, int b, size_t r, int n) {
     std::vector<int> seq1(1, b);
 
     std::cout << "Trivial test" << n << "...";
-    double time = 
+    double time =
         sizeOfLcsIs(seq0.begin(), seq0.end(), seq1.begin(), seq1.end(), r);
     std::cout << " " << time << " secondes" << std::endl;
 }
@@ -112,7 +116,7 @@ void bigTest() {
         seq1[i] = i;
     }
     std::cout << "Big test...";
-    double time = 
+    double time =
         sizeOfLcsIs(seq0.begin(), seq0.end(), seq1.begin(), seq1.end(), 2000);
     std::cout << " " << time << " secondes" << std::endl;
 }
@@ -130,29 +134,29 @@ std::vector<int> randomVector(
     return res;
 }
 
-std::vector<size_t> getLcsIndex(
-    const std::vector<int> & seq0,
-    const std::vector<int> & seq1,
-    const std::vector<int> * indx) {
-
-    if (isCS(seq0, seq1, indx)) {
-        return indx;
-    }
-
-    std::vector<size_t> LCSindx;
-
-    for(size_t i = indx.size(); i > 0; --i) {
-        std::vector<size_t> tindx(indx);
-        tindx.erase(tindx.begin() + i - 1);
-        
-        std::vector<size_t> ans = getLcsIndex(seq0, seq1, tindx);
-        if (ans.size() > LCSindx.size()) {
-            LCSindx = ans;
-        }
-    }
-
-    return LCSindx;
-}
+//std::vector<size_t> getLcsIndex(
+//    const std::vector<int> & seq0,
+//    const std::vector<int> & seq1,
+//    const std::vector<int> * indx) {
+//
+//    if (isCS(seq0, seq1, indx)) {
+//        return indx;
+//    }
+//
+//    std::vector<size_t> LCSindx;
+//
+//    for(size_t i = indx.size(); i > 0; --i) {
+//        std::vector<size_t> tindx(indx);
+//        tindx.erase(tindx.begin() + i - 1);
+//
+//        std::vector<size_t> ans = getLcsIndex(seq0, seq1, tindx);
+//        if (ans.size() > LCSindx.size()) {
+//            LCSindx = ans;
+//        }
+//    }
+//
+//    return LCSindx;
+//}
 
 void testSuite() {
     emptyTest();
