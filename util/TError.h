d09440b154f74914bd3ec8c11a7bb8baf34786fa
orazaev <orazaev@machineGun.(none)>
2012-10-13 14:35:05 +0400

    util/TError: using std::exception

--- util/TError.h
+++ util/TError.h
@@ -1,7 +1,25 @@
 #ifndef TERROR_H
 #define TERROR_H
+#include<string>
+#include<exception>
 
-class TError {
+class TError : public std::exception {
+    std::string msg;
+public:
+    TError() throw()
+        : msg("TError: Something goes wrong!")
+    { }
+
+    virtual ~TError() throw()
+    { }
+
+    TError(const std::string & s) throw()
+        : msg(s)
+    { }
+
+    virtual const char * what() const throw() {
+        return msg.c_str();
+    }
 };
 
 #endif
