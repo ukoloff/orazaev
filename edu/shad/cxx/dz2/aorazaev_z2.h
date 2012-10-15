d30a96083a42b59d9731305904c241db09a6780a
Aman Orazaev <AOrazaev@gmail.com>
2012-10-15 16:17:45 +0400

    shad/cxx/hw2: update

--- edu/shad/cxx/dz2/aorazaev_z2.h
+++ edu/shad/cxx/dz2/aorazaev_z2.h
@@ -32,6 +32,19 @@ public:
     Polynomial<T> & operator += (const Polynomial<T> &);
     Polynomial<T> & operator -= (const Polynomial<T> &);
     Polynomial<T> & operator *= (const Polynomial<T> &);
+    Polynomial<T> operator + (const Polynomial<T> &) const;
+    Polynomial<T> operator - (const Polynomial<T> &) const;
+    Polynomial<T> operator * (const Polynomial<T> &) const;
+
+    T operator() (const T &) const;
+
+    typedef typename std::vector<T>::const_iterator const_iterator;
+    typedef typename std::vector<T>::iterator iterator;
+
+    const_iterator begin() const;
+    const_iterator end() const;
+    iterator begin();
+    iterator end();
 
     template <typename A>
     friend bool operator == (Polynomial<A>, Polynomial<A>);
@@ -46,16 +59,7 @@ public:
 private:
     std::vector<T> coefficients;
 
-    void normalize() {
-        for(size_t x = coefficients.size(); x > 1; --x) {
-            if (std::abs(coefficients[x - 1]) <= EPSILON) {
-                coefficients.pop_back();
-            } else {
-                break;
-            }
-        }
-    }
-
+    void normalize();
 };
 
 
@@ -114,20 +118,76 @@ template <typename T>
 Polynomial<T> & Polynomial<T>::operator *= (const Polynomial<T> & q) {
     size_t oldSize = coefficients.size();
     coefficients.resize(coefficients.size() + q.coefficients.size() - 1, 0);
-    for(int i = coefficients.size(); i >= 0; ++i) {
+
+    for(int i = coefficients.size(); i > 0; --i) {
         T res = 0;
-        for(int j = i; j >= 0; --j) {
+        for(int j = i; j > 0; --j) {
             if (i - j == q.coefficients.size()) {
                 break;
             }
-            res += coefficients[i] * q[i - j];
+            res += coefficients[j - 1] * q[i - j];
         }
-        coefficients[i] = res;
+        coefficients[i - 1] = res;
     }
 
     normalize();
     return *this;
 }
+
+template <typename T>
+Polynomial<T> Polynomial<T>::operator + (const Polynomial<T> & p) const {
+    Polynomial<T> ans(*this);
+    return ans += p;
+}
+
+template <typename T>
+Polynomial<T> Polynomial<T>::operator - (const Polynomial<T> & p) const {
+    Polynomial<T> ans(*this);
+    return ans -= p;
+}
+
+template <typename T>
+Polynomial<T> Polynomial<T>::operator * (const Polynomial<T> & p) const {
+    Polynomial<T> ans(*this);
+    return ans *= p;
+}
+
+template <typename T>
+T Polynomial<T>::operator() (const T & x) const {
+    T res = coefficients[0];
+    T pow = x;
+
+    for(const_iterator it = begin() + 1;
+        it != end(); ++it)
+    {
+        res += *it * pow;
+        pow *= x;
+    }
+
+    return res;
+}
+
+template <typename T>
+typename Polynomial<T>::const_iterator Polynomial<T>::begin() const {
+    return coefficients.begin();
+}
+
+template <typename T>
+typename Polynomial<T>::const_iterator Polynomial<T>::end() const {
+    return coefficients.end();
+}
+
+template <typename T>
+typename Polynomial<T>::iterator Polynomial<T>::begin() {
+    return coefficients.begin();
+}
+
+template <typename T>
+typename Polynomial<T>::iterator Polynomial<T>::end() {
+    return coefficients.end();
+}
+
+
 
 template <typename A>
 bool operator == (Polynomial<A> p, Polynomial<A> q) {
@@ -152,13 +212,15 @@ std::ostream& operator << (
     out << "[";
 #   endif
 
-    for(size_t t = 0; t < p.coefficients.size(); ++t) {
-        out << p[t];
+    for(typename Polynomial<A>::const_iterator it = p.begin();
+        it != p.end(); ++it)
+    {
+        out << *it;
 #       ifndef DEBUG
-        out << " * x^" << t;
+        out << " * x^" << it - p.begin();
 #       endif
 
-        if (t + 1 != p.coefficients.size()) {
+        if (it + 1 != p.end()) {
 #       ifndef DEBUG
             out << " + ";
 #       else
@@ -172,5 +234,17 @@ std::ostream& operator << (
 #   endif
     return out;
 }
+
+template <typename T>
+void Polynomial<T>::normalize() {
+    for(size_t x = coefficients.size(); x > 1; --x) {
+        if (std::abs(coefficients[x - 1]) <= EPSILON) {
+            coefficients.pop_back();
+        } else {
+            break;
+        }
+    }
+}
+
 
 #endif /* AORAZAEV_CXX_DZ2_Z2 */
