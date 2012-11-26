6f1704ff5c220dc7d07d6cfe53fb757e6809dd67
Aman Orazaev <AOrazaev@gmail.com>
2012-11-26 21:11:54 +0400

    edu/shad/cxx: hw3 list of changes:
    [ ] !_1
    [ ] lambda @ value
    [ ] lambda @= value

--- edu/shad/cxx/hw3/lambda.cpp
+++ edu/shad/cxx/hw3/lambda.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <sstream>
 #include <algorithm>
+#include <iterator>
 
 
 
@@ -74,6 +75,13 @@ struct TLambdaGreaterOrEqual {
     }
 };
 
+struct TLambdaNot {
+    template <typename T>
+    bool operator() (const T& rhs) {
+        return !rhs;
+    }
+};
+
 
 
 
@@ -113,6 +121,167 @@ struct TLambdaOutput {
 
 
 
+////////////////////////// Lambda @ Value Functors ////////////////////////////
+
+template <typename T>
+class TLambdaPlusValue {
+    T value;
+    public:
+    TLambdaPlusValue(const T& Value)
+        : value(Value) {
+    }
+
+    template <typename A>
+    A operator()(const A& variable) {
+        return variable + value;
+    }
+};
+
+template <typename T>
+class TLambdaMinusValue {
+    T value;
+    public:
+    TLambdaMinusValue(const T& Value)
+        : value(Value) {
+    }
+
+    template <typename A>
+    A operator()(const A& variable) {
+        return variable - value;
+    }
+};
+
+template <typename T>
+class TLambdaTimesValue {
+    T value;
+    public:
+    TLambdaTimesValue(const T& Value)
+        : value(Value) {
+    }
+
+    template <typename A>
+    A operator()(const A& variable) {
+        return variable * value;
+    }
+};
+
+template <typename T>
+class TLambdaDivValue {
+    T value;
+    public:
+    TLambdaDivValue(const T& Value)
+        : value(Value) {
+    }
+
+    template <typename A>
+    A operator()(const A& variable) {
+        return variable / value;
+    }
+};
+
+template <typename T>
+class TLambdaModValue {
+    T value;
+    public:
+    TLambdaModValue(const T& Value)
+        : value(Value) {
+    }
+
+    template <typename A>
+    A operator()(const A& variable) {
+        return variable % value;
+    }
+};
+
+
+
+
+
+
+
+////////////////////////// Lambda @ Value Functors ////////////////////////////
+
+template <typename T>
+class TLambdaEqualPlus {
+    T value;
+    public:
+    TLambdaEqualPlus(const T& Value)
+        : value(Value) {
+    }
+
+    template <typename A>
+    A& operator() (A& variable) {
+        variable += value;
+        return variable;
+    }
+};
+
+template <typename T>
+class TLambdaEqualMinus {
+    T value;
+    public:
+    TLambdaEqualMinus(const T& Value)
+        : value(Value) {
+    }
+
+    template <typename A>
+    A& operator() (A& variable) {
+        variable -= value;
+        return variable;
+    }
+};
+
+template <typename T>
+class TLambdaEqualTimes {
+    T value;
+    public:
+    TLambdaEqualTimes(const T& Value)
+        : value(Value) {
+    }
+
+    template <typename A>
+    A& operator() (A& variable) {
+        variable *= value;
+        return variable;
+    }
+};
+
+template <typename T>
+class TLambdaEqualDiv {
+    T value;
+    public:
+    TLambdaEqualDiv(const T& Value)
+        : value(Value) {
+    }
+
+    template <typename A>
+    A& operator() (A& variable) {
+        variable /= value;
+        return variable;
+    }
+};
+
+template <typename T>
+class TLambdaEqualMod {
+    T value;
+    public:
+    TLambdaEqualMod(const T& Value)
+        : value(Value) {
+    }
+
+    template <typename A>
+    A& operator() (A& variable) {
+        variable %= value;
+        return variable;
+    }
+};
+
+
+
+
+
+
+
 ////////////////////////////// Lambda parameters //////////////////////////////
 
 struct TLambdaFirstParameter {
@@ -120,7 +289,60 @@ struct TLambdaFirstParameter {
     TLambdaSetTo<T> operator = (T Value) {
         return TLambdaSetTo<T>(Value);
     }
+
+    template <typename T>
+    TLambdaPlusValue<T> operator+ (const T& value) {
+        return TLambdaPlusValue<T>(value);
+    }
+
+    template <typename T>
+    TLambdaMinusValue<T> operator- (const T& value) {
+        return TLambdaMinusValue<T>(value);
+    }
+
+    template <typename T>
+    TLambdaTimesValue<T> operator* (const T& value) {
+        return TLambdaTimesValue<T>(value);
+    }
+
+    template <typename T>
+    TLambdaDivValue<T> operator/ (const T& value) {
+        return TLambdaDivValue<T>(value);
+    }
+
+    template <typename T>
+    TLambdaModValue<T> operator% (const T& value) {
+        return TLambdaModValue<T>(value);
+    }
+
+    template <typename T>
+    TLambdaEqualPlus<T> operator+= (const T& value) {
+        return TLambdaEqualPlus<T>(value);
+    }
+
+    template <typename T>
+    TLambdaEqualMinus<T> operator-= (const T& value) {
+        return TLambdaEqualMinus<T>(value);
+    }
+
+    template <typename T>
+    TLambdaEqualTimes<T> operator*= (const T& value) {
+        return TLambdaEqualTimes<T>(value);
+    }
+
+    template <typename T>
+    TLambdaEqualDiv<T> operator /= (const T& value) {
+        return TLambdaEqualDiv<T>(value);
+    }
+
+    template <typename T>
+    TLambdaEqualMod<T> operator %= (const T& value) {
+        return TLambdaEqualMod<T>(value);
+    }
 };
+
+
+
 
 
 
@@ -204,6 +426,9 @@ TLambdaGreaterOrEqual operator<= (
     return TLambdaGreaterOrEqual();
 }
 
+TLambdaNot operator! (const TLambdaFirstParameter& first) {
+    return TLambdaNot();
+}
 
 
 
@@ -214,6 +439,10 @@ TLambdaOutput operator<< (
 ) {
     return TLambdaOutput(out);
 }
+
+
+
+
 
 
 
@@ -229,13 +458,16 @@ int main() {
     };
 
     int bar[] = {
-        50, 32, 12, 31, 28
+        50, 32, 0, 31, 28
     };
+
+
 
     printElements(foo, foo + 5);
 
     std::for_each(foo, foo + 5, _1 = 100);
     printElements(foo, foo + 5);
+
 
 
     std::sort(bar, bar + 5, _1 < _2);
@@ -264,8 +496,81 @@ int main() {
     printElements(bar, bar + 5);
 
 
+
     std::for_each(foo, foo + 5, std::cout << _1 << ", ");
     std::cout << std::endl;
+
+
+
+    std::transform(
+        bar,
+        bar + 5,
+        std::ostream_iterator<bool>(std::cout, " "),
+        !_1
+    );
+    std::cout << std::endl;
+
+
+
+    std::transform(
+        bar,
+        bar + 5,
+        std::ostream_iterator<int>(std::cout, " "),
+        _1 / 2
+    );
+    std::cout << std::endl;
+
+    std::transform(
+        bar,
+        bar + 5,
+        std::ostream_iterator<int>(std::cout, " "),
+        _1 + 1000
+    );
+    std::cout << std::endl;
+
+    std::transform(
+        bar,
+        bar + 5,
+        std::ostream_iterator<int>(std::cout, " "),
+        _1 * 10
+    );
+    std::cout << std::endl;
+
+    std::transform(
+        bar,
+        bar + 5,
+        std::ostream_iterator<int>(std::cout, " "),
+        _1 - 100
+    );
+    std::cout << std::endl;
+
+    std::transform(
+        bar,
+        bar + 5,
+        std::ostream_iterator<int>(std::cout, " "),
+        _1 % 10
+    );
+    std::cout << std::endl;
+
+
+
+    std::for_each(bar, bar + 5, _1 += 100);
+    printElements(bar, bar + 5);
+
+    std::for_each(bar, bar + 5, _1 -= 100);
+    printElements(bar, bar + 5);
+
+    std::for_each(bar, bar + 5, _1 *= 10);
+    printElements(bar, bar + 5);
+
+    std::for_each(bar, bar + 5, _1 /= 10);
+    printElements(bar, bar + 5);
+
+    std::for_each(foo, foo + 5, _1 %= 13);
+    printElements(foo, foo + 5);
+
+
+
 
     return 0;
 }
