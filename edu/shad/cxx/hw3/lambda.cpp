fb98eefd522604fd050be52ab3c38f189a3bf539
Aman Orazaev <AOrazaev@gmail.com>
2012-12-07 21:20:32 +0400

    edu/shad/cxx: hw3 complete

--- edu/shad/cxx/hw3/lambda.cpp
+++ edu/shad/cxx/hw3/lambda.cpp
@@ -199,7 +199,7 @@ class TLambdaModValue {
 
 
 
-////////////////////////// Lambda @ Value Functors ////////////////////////////
+////////////////////////// Lambda @= Value Functors ///////////////////////////
 
 template <typename T>
 class TLambdaEqualPlus {
@@ -282,6 +282,64 @@ class TLambdaEqualMod {
 
 
 
+
+
+/////////////////////////////// @Lambda Functors //////////////////////////////
+
+struct TLambdaUnaryMinus {
+    template <typename T>
+    T operator() (const T& variable) {
+        return -variable;
+    }
+};
+
+struct TLambdaUnaryPlus {
+    template <typename T>
+    T operator() (const T& variable) {
+        return +variable;
+    }
+};
+
+struct TLambdaUnaryStar {
+    template <typename T>
+    T operator() (const T* const variable) {
+        return *variable;
+    }
+};
+
+struct TLambdaPrefixIncrement {
+    template <typename T>
+    T operator() (T& variable) {
+        return ++variable;
+    }
+};
+
+struct TLambdaPostfixIncrement {
+    template <typename T>
+    T operator() (T& variable) {
+        return variable++;
+    }
+};
+
+struct TLambdaPrefixDecrement {
+    template <typename T>
+    T operator() (T& variable) {
+        return --variable;
+    }
+};
+
+struct TLambdaPostfixDecrement {
+    template <typename T>
+    T operator() (T& variable) {
+        return variable--;
+    }
+};
+
+
+
+
+
+
 ////////////////////////////// Lambda parameters //////////////////////////////
 
 struct TLambdaFirstParameter {
@@ -295,14 +353,26 @@ struct TLambdaFirstParameter {
         return TLambdaPlusValue<T>(value);
     }
 
+    TLambdaUnaryPlus operator+ () {
+        return TLambdaUnaryPlus();
+    }
+
     template <typename T>
     TLambdaMinusValue<T> operator- (const T& value) {
         return TLambdaMinusValue<T>(value);
     }
 
+    TLambdaUnaryMinus operator-() {
+        return TLambdaUnaryMinus();
+    }
+
     template <typename T>
     TLambdaTimesValue<T> operator* (const T& value) {
         return TLambdaTimesValue<T>(value);
+    }
+
+    TLambdaUnaryStar operator* () {
+        return TLambdaUnaryStar();
     }
 
     template <typename T>
@@ -338,6 +408,22 @@ struct TLambdaFirstParameter {
     template <typename T>
     TLambdaEqualMod<T> operator %= (const T& value) {
         return TLambdaEqualMod<T>(value);
+    }
+
+    TLambdaPrefixIncrement operator++ () {
+        return TLambdaPrefixIncrement();
+    }
+
+    TLambdaPrefixDecrement operator-- () {
+        return TLambdaPrefixDecrement();
+    }
+
+    TLambdaPostfixIncrement operator++ (int) {
+        return TLambdaPostfixIncrement();
+    }
+
+    TLambdaPostfixDecrement operator-- (int) {
+        return TLambdaPostfixDecrement();
     }
 };
 
@@ -463,10 +549,14 @@ int main() {
 
 
 
+
+
     printElements(foo, foo + 5);
 
     std::for_each(foo, foo + 5, _1 = 100);
     printElements(foo, foo + 5);
+
+
 
 
 
@@ -483,6 +573,8 @@ int main() {
     printElements(bar, bar + 5);
 
 
+
+
     std::sort(bar, bar + 5, _1 <= _2);
     printElements(bar, bar + 5);
 
@@ -497,8 +589,10 @@ int main() {
 
 
 
+
     std::for_each(foo, foo + 5, std::cout << _1 << ", ");
     std::cout << std::endl;
+
 
 
 
@@ -509,6 +603,7 @@ int main() {
         !_1
     );
     std::cout << std::endl;
+
 
 
 
@@ -554,6 +649,44 @@ int main() {
 
 
 
+
+    std::transform(
+        bar,
+        bar + 5,
+        std::ostream_iterator<int>(std::cout, " "),
+        -_1
+    );
+    std::cout << std::endl;
+
+    std::transform(
+        bar,
+        bar + 5,
+        std::ostream_iterator<int>(std::cout, " "),
+        +_1
+    );
+    std::cout << std::endl;
+
+
+
+
+    int* baz[5];
+    baz[0] = bar;
+    baz[1] = bar + 1;
+    baz[2] = bar + 2;
+    baz[3] = bar + 3;
+    baz[4] = bar + 4;
+
+    std::transform(
+        baz,
+        baz + 5,
+        std::ostream_iterator<int>(std::cout, " "),
+        *_1
+    );
+    std::cout << std::endl;
+
+
+
+
     std::for_each(bar, bar + 5, _1 += 100);
     printElements(bar, bar + 5);
 
@@ -569,6 +702,42 @@ int main() {
     std::for_each(foo, foo + 5, _1 %= 13);
     printElements(foo, foo + 5);
 
+
+
+
+    std::transform(
+        bar,
+        bar + 5,
+        std::ostream_iterator<int>(std::cout, " "),
+        _1++
+    );
+    std::cout << std::endl;
+    printElements(bar, bar + 5);
+
+    std::transform(
+        bar,
+        bar + 5,
+        std::ostream_iterator<int>(std::cout, " "),
+        --_1
+    );
+    std::cout << std::endl;
+
+    std::transform(
+        bar,
+        bar + 5,
+        std::ostream_iterator<int>(std::cout, " "),
+        _1--
+    );
+    std::cout << std::endl;
+    printElements(bar, bar + 5);
+
+    std::transform(
+        bar,
+        bar + 5,
+        std::ostream_iterator<int>(std::cout, " "),
+        ++_1
+    );
+    std::cout << std::endl;
 
 
 
