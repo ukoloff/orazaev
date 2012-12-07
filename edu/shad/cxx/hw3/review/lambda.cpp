0dd6afcbe51bab319bdaad8b3dcf6a552d69f80b
Aman Orazaev <AOrazaev@gmail.com>
2012-12-07 21:40:45 +0400

    review commit

---
+++ edu/shad/cxx/hw3/review/lambda.cpp
@@ -0,0 +1,752 @@
+/*
+ *      C++ homework. Problem 3.
+ *
+ *      Due date: 07.12.12
+ *
+ *      Author: Aman Orazaev
+ */
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+
+
+
+
+template <typename T>
+void printElements(T begin, T end) {
+    while (begin != end) {
+        std::cout << *begin++ << " ";
+    }
+    std::cout << std::endl;
+}
+
+
+
+
+
+//////////////////////////////// Set functors /////////////////////////////////
+
+template <typename T>
+class TLambdaSetTo {
+    T value;
+    public:
+    TLambdaSetTo(const T& Value)
+        : value(Value) {
+    }
+
+    T& operator() (T& variable) const {
+        variable = value;
+    }
+};
+
+
+
+
+
+
+////////////////////////////// Logical functors ///////////////////////////////
+
+struct TLambdaLess {
+    template <typename T>
+    bool operator() (const T& lhs, const T& rhs) const {
+        return lhs < rhs;
+    }
+};
+
+struct TLambdaGreater {
+    template <typename T>
+    bool operator() (const T& lhs, const T& rhs) const {
+        return lhs > rhs;
+    }
+};
+
+struct TLambdaEqual {
+    template <typename T>
+    bool operator() (const T& lhs, const T& rhs) const {
+        return lhs == rhs;
+    }
+};
+
+struct TLambdaLessOrEqual {
+    template <typename T>
+    bool operator() (const T& lhs, const T& rhs) const {
+        return lhs <= rhs;
+    }
+};
+
+struct TLambdaGreaterOrEqual {
+    template <typename T>
+    bool operator() (const T& lhs, const T& rhs) const {
+        return lhs >= rhs;
+    }
+};
+
+struct TLambdaNot {
+    template <typename T>
+    bool operator() (const T& rhs) {
+        return !rhs;
+    }
+};
+
+
+
+
+
+
+/////////////////////////////// Output functor ////////////////////////////////
+
+struct TLambdaOutput {
+    std::ostream& out;
+    std::stringstream accumulated;
+    public:
+    TLambdaOutput(std::ostream& Out)
+        : out(Out) {
+    }
+
+    TLambdaOutput(const TLambdaOutput& other)
+        : out(other.out)
+        , accumulated(other.accumulated.str()) {
+    }
+
+    template <typename T>
+    TLambdaOutput& operator<< (const T& rhs) {
+        accumulated << rhs;
+        return *this;
+    }
+
+    template <typename T>
+    std::ostream& operator() (const T& value) {
+        out << value << accumulated.str();
+        return out;
+    }
+};
+
+
+
+
+
+
+
+////////////////////////// Lambda @ Value Functors ////////////////////////////
+
+template <typename T>
+class TLambdaPlusValue {
+    T value;
+    public:
+    TLambdaPlusValue(const T& Value)
+        : value(Value) {
+    }
+
+    template <typename A>
+    A operator()(const A& variable) {
+        return variable + value;
+    }
+};
+
+template <typename T>
+class TLambdaMinusValue {
+    T value;
+    public:
+    TLambdaMinusValue(const T& Value)
+        : value(Value) {
+    }
+
+    template <typename A>
+    A operator()(const A& variable) {
+        return variable - value;
+    }
+};
+
+template <typename T>
+class TLambdaTimesValue {
+    T value;
+    public:
+    TLambdaTimesValue(const T& Value)
+        : value(Value) {
+    }
+
+    template <typename A>
+    A operator()(const A& variable) {
+        return variable * value;
+    }
+};
+
+template <typename T>
+class TLambdaDivValue {
+    T value;
+    public:
+    TLambdaDivValue(const T& Value)
+        : value(Value) {
+    }
+
+    template <typename A>
+    A operator()(const A& variable) {
+        return variable / value;
+    }
+};
+
+template <typename T>
+class TLambdaModValue {
+    T value;
+    public:
+    TLambdaModValue(const T& Value)
+        : value(Value) {
+    }
+
+    template <typename A>
+    A operator()(const A& variable) {
+        return variable % value;
+    }
+};
+
+
+
+
+
+
+
+////////////////////////// Lambda @= Value Functors ///////////////////////////
+
+template <typename T>
+class TLambdaEqualPlus {
+    T value;
+    public:
+    TLambdaEqualPlus(const T& Value)
+        : value(Value) {
+    }
+
+    template <typename A>
+    A& operator() (A& variable) {
+        variable += value;
+        return variable;
+    }
+};
+
+template <typename T>
+class TLambdaEqualMinus {
+    T value;
+    public:
+    TLambdaEqualMinus(const T& Value)
+        : value(Value) {
+    }
+
+    template <typename A>
+    A& operator() (A& variable) {
+        variable -= value;
+        return variable;
+    }
+};
+
+template <typename T>
+class TLambdaEqualTimes {
+    T value;
+    public:
+    TLambdaEqualTimes(const T& Value)
+        : value(Value) {
+    }
+
+    template <typename A>
+    A& operator() (A& variable) {
+        variable *= value;
+        return variable;
+    }
+};
+
+template <typename T>
+class TLambdaEqualDiv {
+    T value;
+    public:
+    TLambdaEqualDiv(const T& Value)
+        : value(Value) {
+    }
+
+    template <typename A>
+    A& operator() (A& variable) {
+        variable /= value;
+        return variable;
+    }
+};
+
+template <typename T>
+class TLambdaEqualMod {
+    T value;
+    public:
+    TLambdaEqualMod(const T& Value)
+        : value(Value) {
+    }
+
+    template <typename A>
+    A& operator() (A& variable) {
+        variable %= value;
+        return variable;
+    }
+};
+
+
+
+
+
+
+
+
+
+/////////////////////////////// @Lambda Functors //////////////////////////////
+
+struct TLambdaUnaryMinus {
+    template <typename T>
+    T operator() (const T& variable) {
+        return -variable;
+    }
+};
+
+struct TLambdaUnaryPlus {
+    template <typename T>
+    T operator() (const T& variable) {
+        return +variable;
+    }
+};
+
+struct TLambdaUnaryStar {
+    template <typename T>
+    T operator() (const T* const variable) {
+        return *variable;
+    }
+};
+
+struct TLambdaPrefixIncrement {
+    template <typename T>
+    T operator() (T& variable) {
+        return ++variable;
+    }
+};
+
+struct TLambdaPostfixIncrement {
+    template <typename T>
+    T operator() (T& variable) {
+        return variable++;
+    }
+};
+
+struct TLambdaPrefixDecrement {
+    template <typename T>
+    T operator() (T& variable) {
+        return --variable;
+    }
+};
+
+struct TLambdaPostfixDecrement {
+    template <typename T>
+    T operator() (T& variable) {
+        return variable--;
+    }
+};
+
+
+
+
+
+
+////////////////////////////// Lambda parameters //////////////////////////////
+
+struct TLambdaFirstParameter {
+    template <typename T>
+    TLambdaSetTo<T> operator = (T Value) {
+        return TLambdaSetTo<T>(Value);
+    }
+
+    template <typename T>
+    TLambdaPlusValue<T> operator+ (const T& value) {
+        return TLambdaPlusValue<T>(value);
+    }
+
+    TLambdaUnaryPlus operator+ () {
+        return TLambdaUnaryPlus();
+    }
+
+    template <typename T>
+    TLambdaMinusValue<T> operator- (const T& value) {
+        return TLambdaMinusValue<T>(value);
+    }
+
+    TLambdaUnaryMinus operator-() {
+        return TLambdaUnaryMinus();
+    }
+
+    template <typename T>
+    TLambdaTimesValue<T> operator* (const T& value) {
+        return TLambdaTimesValue<T>(value);
+    }
+
+    TLambdaUnaryStar operator* () {
+        return TLambdaUnaryStar();
+    }
+
+    template <typename T>
+    TLambdaDivValue<T> operator/ (const T& value) {
+        return TLambdaDivValue<T>(value);
+    }
+
+    template <typename T>
+    TLambdaModValue<T> operator% (const T& value) {
+        return TLambdaModValue<T>(value);
+    }
+
+    template <typename T>
+    TLambdaEqualPlus<T> operator+= (const T& value) {
+        return TLambdaEqualPlus<T>(value);
+    }
+
+    template <typename T>
+    TLambdaEqualMinus<T> operator-= (const T& value) {
+        return TLambdaEqualMinus<T>(value);
+    }
+
+    template <typename T>
+    TLambdaEqualTimes<T> operator*= (const T& value) {
+        return TLambdaEqualTimes<T>(value);
+    }
+
+    template <typename T>
+    TLambdaEqualDiv<T> operator /= (const T& value) {
+        return TLambdaEqualDiv<T>(value);
+    }
+
+    template <typename T>
+    TLambdaEqualMod<T> operator %= (const T& value) {
+        return TLambdaEqualMod<T>(value);
+    }
+
+    TLambdaPrefixIncrement operator++ () {
+        return TLambdaPrefixIncrement();
+    }
+
+    TLambdaPrefixDecrement operator-- () {
+        return TLambdaPrefixDecrement();
+    }
+
+    TLambdaPostfixIncrement operator++ (int) {
+        return TLambdaPostfixIncrement();
+    }
+
+    TLambdaPostfixDecrement operator-- (int) {
+        return TLambdaPostfixDecrement();
+    }
+};
+
+
+
+
+
+
+struct TLambdaSecondParameter {
+};
+
+
+
+
+
+
+////////////////////////////////// Operators //////////////////////////////////
+
+TLambdaLess operator< (
+    const TLambdaFirstParameter& first,
+    const TLambdaSecondParameter& second
+) {
+    return TLambdaLess();
+}
+
+TLambdaLess operator> (
+    const TLambdaSecondParameter& second,
+    const TLambdaFirstParameter& first
+) {
+    return TLambdaLess();
+}
+
+TLambdaGreater operator> (
+    const TLambdaFirstParameter& first,
+    const TLambdaSecondParameter& second
+) {
+    return TLambdaGreater();
+}
+
+TLambdaGreater operator< (
+    const TLambdaSecondParameter& second,
+    const TLambdaFirstParameter& first
+) {
+    return TLambdaGreater();
+}
+
+TLambdaEqual operator== (
+    const TLambdaFirstParameter& first,
+    const TLambdaSecondParameter& second
+) {
+    return TLambdaEqual();
+}
+
+TLambdaEqual operator== (
+    const TLambdaSecondParameter& second,
+    const TLambdaFirstParameter& first
+) {
+    return TLambdaEqual();
+}
+
+TLambdaLessOrEqual operator<= (
+    const TLambdaFirstParameter& first,
+    const TLambdaSecondParameter& second
+) {
+    return TLambdaLessOrEqual();
+}
+
+TLambdaLessOrEqual operator>= (
+    const TLambdaSecondParameter& second,
+    const TLambdaFirstParameter& first
+) {
+    return TLambdaLessOrEqual();
+}
+
+TLambdaGreaterOrEqual operator>= (
+    const TLambdaFirstParameter& first,
+    const TLambdaSecondParameter& second
+) {
+    return TLambdaGreaterOrEqual();
+}
+
+TLambdaGreaterOrEqual operator<= (
+    const TLambdaSecondParameter& second,
+    const TLambdaFirstParameter& first
+) {
+    return TLambdaGreaterOrEqual();
+}
+
+TLambdaNot operator! (const TLambdaFirstParameter& first) {
+    return TLambdaNot();
+}
+
+
+
+
+TLambdaOutput operator<< (
+    std::ostream& out,
+    const TLambdaFirstParameter& first
+) {
+    return TLambdaOutput(out);
+}
+
+
+
+
+
+
+
+
+/////////////////////////////// Demonstration /////////////////////////////////
+
+int main() {
+    TLambdaFirstParameter _1;
+    TLambdaSecondParameter _2;
+
+    int foo[] = {
+        2, 4, 12, 27, 15
+    };
+
+    int bar[] = {
+        50, 32, 0, 31, 28
+    };
+
+
+
+
+
+    printElements(foo, foo + 5);
+
+    std::for_each(foo, foo + 5, _1 = 100);
+    printElements(foo, foo + 5);
+
+
+
+
+
+    std::sort(bar, bar + 5, _1 < _2);
+    printElements(bar, bar + 5);
+
+    std::sort(bar, bar + 5, _2 < _1);
+    printElements(bar, bar + 5);
+
+    std::sort(bar, bar + 5, _2 > _1);
+    printElements(bar, bar + 5);
+
+    std::sort(bar, bar + 5, _1 > _2);
+    printElements(bar, bar + 5);
+
+
+
+
+    std::sort(bar, bar + 5, _1 <= _2);
+    printElements(bar, bar + 5);
+
+    std::sort(bar, bar + 5, _2 <= _1);
+    printElements(bar, bar + 5);
+
+    std::sort(bar, bar + 5, _2 >= _1);
+    printElements(bar, bar + 5);
+
+    std::sort(bar, bar + 5, _1 >= _2);
+    printElements(bar, bar + 5);
+
+
+
+
+    std::for_each(foo, foo + 5, std::cout << _1 << ", ");
+    std::cout << std::endl;
+
+
+
+
+    std::transform(
+        bar,
+        bar + 5,
+        std::ostream_iterator<bool>(std::cout, " "),
+        !_1
+    );
+    std::cout << std::endl;
+
+
+
+
+    std::transform(
+        bar,
+        bar + 5,
+        std::ostream_iterator<int>(std::cout, " "),
+        _1 / 2
+    );
+    std::cout << std::endl;
+
+    std::transform(
+        bar,
+        bar + 5,
+        std::ostream_iterator<int>(std::cout, " "),
+        _1 + 1000
+    );
+    std::cout << std::endl;
+
+    std::transform(
+        bar,
+        bar + 5,
+        std::ostream_iterator<int>(std::cout, " "),
+        _1 * 10
+    );
+    std::cout << std::endl;
+
+    std::transform(
+        bar,
+        bar + 5,
+        std::ostream_iterator<int>(std::cout, " "),
+        _1 - 100
+    );
+    std::cout << std::endl;
+
+    std::transform(
+        bar,
+        bar + 5,
+        std::ostream_iterator<int>(std::cout, " "),
+        _1 % 10
+    );
+    std::cout << std::endl;
+
+
+
+
+    std::transform(
+        bar,
+        bar + 5,
+        std::ostream_iterator<int>(std::cout, " "),
+        -_1
+    );
+    std::cout << std::endl;
+
+    std::transform(
+        bar,
+        bar + 5,
+        std::ostream_iterator<int>(std::cout, " "),
+        +_1
+    );
+    std::cout << std::endl;
+
+
+
+
+    int* baz[5];
+    baz[0] = bar;
+    baz[1] = bar + 1;
+    baz[2] = bar + 2;
+    baz[3] = bar + 3;
+    baz[4] = bar + 4;
+
+    std::transform(
+        baz,
+        baz + 5,
+        std::ostream_iterator<int>(std::cout, " "),
+        *_1
+    );
+    std::cout << std::endl;
+
+
+
+
+    std::for_each(bar, bar + 5, _1 += 100);
+    printElements(bar, bar + 5);
+
+    std::for_each(bar, bar + 5, _1 -= 100);
+    printElements(bar, bar + 5);
+
+    std::for_each(bar, bar + 5, _1 *= 10);
+    printElements(bar, bar + 5);
+
+    std::for_each(bar, bar + 5, _1 /= 10);
+    printElements(bar, bar + 5);
+
+    std::for_each(foo, foo + 5, _1 %= 13);
+    printElements(foo, foo + 5);
+
+
+
+
+    std::transform(
+        bar,
+        bar + 5,
+        std::ostream_iterator<int>(std::cout, " "),
+        _1++
+    );
+    std::cout << std::endl;
+    printElements(bar, bar + 5);
+
+    std::transform(
+        bar,
+        bar + 5,
+        std::ostream_iterator<int>(std::cout, " "),
+        --_1
+    );
+    std::cout << std::endl;
+
+    std::transform(
+        bar,
+        bar + 5,
+        std::ostream_iterator<int>(std::cout, " "),
+        _1--
+    );
+    std::cout << std::endl;
+    printElements(bar, bar + 5);
+
+    std::transform(
+        bar,
+        bar + 5,
+        std::ostream_iterator<int>(std::cout, " "),
+        ++_1
+    );
+    std::cout << std::endl;
+
+
+
+    return 0;
+}
---
+++ edu/shad/cxx/hw3/review/scalar.cpp
@@ -0,0 +1,331 @@
+/*
+ *      C++ homework. Problem 2.
+ *
+ *      Due date: 07.12.12
+ *
+ *      Author: Aman Orazaev
+ *
+ *
+ *
+ *      My timing results for 10000000 scalar products for
+ *      vector of 20 elements. (copy paste from stdout):
+ *      --------------------------------------------------
+ *      function scalar_product<N> speed: 0.44
+ *      class TFastScalarProduct<N> speed: 0.4
+ *      struct TNativeScalarProduct<N> speed: 0.87
+ *      Cycle scalar product speed: 0.7
+ *      Native sum of 20 elements speed: 0.02
+ *      --------------------------------------------------
+ *
+ *      Without any additional options for g++ (4.6.1).
+ */
+#include <time.h>
+#include <cstdlib>
+#include <unistd.h>
+
+#include <iostream>
+
+template <size_t N>
+class TFastScalarProduct;
+
+template <size_t N>
+struct TNativeScalarProduct;
+
+
+
+template <size_t N, typename T>
+inline T scalar_product(T* first, T* second) {
+    return TFastScalarProduct<N>().product(first, second);
+}
+
+
+
+
+////////////////////////////// TNativeScalarProduct ///////////////////////////
+
+template <size_t N>
+struct TNativeScalarProduct {
+    template <typename T>
+    inline T operator () (T* first, T* second) {
+        return first[N - 1] * second[N - 1] +
+            TNativeScalarProduct<N - 1>()(first, second);
+    }
+};
+
+
+
+template <>
+struct TNativeScalarProduct<1> {
+    template <typename T>
+    inline T operator () (T* first, T* second) {
+        return first[0] * second[0];
+    }
+};
+
+
+
+////////////////////////////// TFastScalarProduct /////////////////////////////
+
+template <size_t N>
+class TFastScalarProduct {
+    template <typename T>
+    inline T prod10(T* a, T* b) const {
+        return
+        a[N - 1] * b[N - 1] + a[N - 2] * b[N - 2] +
+        a[N - 3] * b[N - 3] + a[N - 4] * b[N - 4] +
+        a[N - 5] * b[N - 5] + a[N - 6] * b[N - 6] +
+        a[N - 7] * b[N - 7] + a[N - 8] * b[N - 8] +
+        a[N - 9] * b[N - 9] + a[N - 10] * b[N - 10];
+    }
+
+    public:
+    template <typename T>
+    inline T product(T* foo, T* bar) const {
+        return prod10(foo, bar) + TFastScalarProduct<N - 10>().product(foo, bar);
+    }
+};
+
+template <>
+class TFastScalarProduct<9> {
+    public:
+    template <typename T>
+    inline T product(T* foo, T* bar) const {
+        return
+        foo[0] * bar[0] + foo[1] * bar[1] +
+        foo[2] * bar[2] + foo[3] * bar[3] +
+        foo[4] * bar[4] + foo[5] * bar[5] +
+        foo[6] * bar[6] + foo[7] * bar[7] +
+        foo[8] * bar[8];
+    }
+};
+
+template <>
+class TFastScalarProduct<8> {
+    public:
+    template <typename T>
+    inline T product(T* foo, T* bar) const {
+        return
+        foo[0] * bar[0] + foo[1] * bar[1] +
+        foo[2] * bar[2] + foo[3] * bar[3] +
+        foo[4] * bar[4] + foo[5] * bar[5] +
+        foo[6] * bar[6] + foo[7] * bar[7];
+    }
+};
+
+template <>
+class TFastScalarProduct<7> {
+    public:
+    template <typename T>
+    inline T product(T* foo, T* bar) const {
+        return
+        foo[0] * bar[0] + foo[1] * bar[1] +
+        foo[2] * bar[2] + foo[3] * bar[3] +
+        foo[4] * bar[4] + foo[5] * bar[5] +
+        foo[6] * bar[6];
+    }
+};
+
+template <>
+class TFastScalarProduct<6> {
+    public:
+    template <typename T>
+    inline T product(T* foo, T* bar) const {
+        return
+        foo[0] * bar[0] + foo[1] * bar[1] +
+        foo[2] * bar[2] + foo[3] * bar[3] +
+        foo[4] * bar[4] + foo[5] * bar[5];
+    }
+};
+
+template <>
+class TFastScalarProduct<5> {
+    public:
+    template <typename T>
+    inline T product(T* foo, T* bar) const {
+        return
+        foo[0] * bar[0] + foo[1] * bar[1] +
+        foo[2] * bar[2] + foo[3] * bar[3] +
+        foo[4] * bar[4];
+    }
+};
+
+template <>
+class TFastScalarProduct<4> {
+    public:
+    template <typename T>
+    inline T product(T* foo, T* bar) const {
+        return
+        foo[0] * bar[0] + foo[1] * bar[1] +
+        foo[2] * bar[2] + foo[3] * bar[3];
+    }
+};
+
+template <>
+class TFastScalarProduct<3> {
+    public:
+    template <typename T>
+    inline T product(T* foo, T* bar) const {
+        return
+        foo[0] * bar[0] + foo[1] * bar[1] +
+        foo[2] * bar[2];
+    }
+};
+
+template <>
+class TFastScalarProduct<2> {
+    public:
+    template <typename T>
+    inline T product(T* foo, T* bar) const {
+        return foo[0] * bar[0] + foo[1] * bar[1];
+    }
+};
+
+template <>
+class TFastScalarProduct<1> {
+    public:
+    template <typename T>
+    inline T product(T* foo, T* bar) const {
+        return foo[0] * bar[0];
+    }
+};
+
+template <>
+class TFastScalarProduct<0> {
+    public:
+    template <typename T>
+    inline T product(T* foo, T* bar) const {
+        return 0;
+    }
+};
+
+
+
+///////////////////////////////// TTimer //////////////////////////////////////
+
+class TTimer {
+    clock_t startClocks;
+    clock_t endClocks;
+    public:
+    TTimer()
+        : startClocks(0)
+        , endClocks(0) {
+    }
+
+    void start() {
+        startClocks = clock();
+    }
+
+    void stop() {
+        endClocks = clock();
+    }
+
+    double getSeconds() const {
+        return static_cast<double>(
+            endClocks - startClocks
+        ) / CLOCKS_PER_SEC;
+    }
+
+    clock_t getClocks() const {
+        return endClocks - startClocks;
+    }
+};
+
+
+
+static const size_t vectorSize = 20;
+static const size_t numberOfStarts = 10000000;
+
+
+
+int main() {
+    TTimer timer;
+
+
+    int foo[vectorSize];
+    int bar[vectorSize];
+    for (size_t i = 0; i < vectorSize; ++i) {
+        foo[i] = bar[i] = 1;
+    }
+
+
+
+
+    timer.start();
+    for (size_t runNo = 0; runNo < numberOfStarts; ++runNo) {
+        scalar_product<vectorSize>(foo, bar);
+    }
+    timer.stop();
+
+    std::cout << "function scalar_product<N> speed: "
+              << timer.getSeconds() << std::endl;
+
+
+
+
+
+    timer.start();
+    for (size_t runNo = 0; runNo < numberOfStarts; ++runNo) {
+        TFastScalarProduct<vectorSize>().product(foo, bar);
+    }
+    timer.stop();
+
+    std::cout << "class TFastScalarProduct<N> speed: "
+              << timer.getSeconds() << std::endl;
+
+
+
+
+
+    timer.start();
+    for (size_t runNo = 0; runNo < numberOfStarts; ++runNo) {
+        TNativeScalarProduct<vectorSize>()(foo, bar);
+    }
+    timer.stop();
+
+    std::cout << "struct TNativeScalarProduct<N> speed: "
+              << timer.getSeconds() << std::endl;
+
+
+
+
+
+    timer.start();
+    int cycle_scalar_product;
+    for (size_t runNo = 0; runNo < numberOfStarts; ++runNo) {
+        cycle_scalar_product = 0;
+        for (size_t index = 0; index < vectorSize; ++index) {
+            cycle_scalar_product += foo[index] * bar[index];
+        }
+    }
+    timer.stop();
+
+    std::cout << "Cycle scalar product speed: "
+              << timer.getSeconds() << std::endl;
+
+
+
+
+
+    timer.start();
+    for (size_t runNo = 0; runNo < numberOfStarts; ++runNo) {
+        foo[0] * bar[0] + foo[1] * bar[1] +
+        foo[2] * bar[2] + foo[3] * bar[3] +
+        foo[4] * bar[4] + foo[5] * bar[5] +
+        foo[6] * bar[6] + foo[7] * bar[7] +
+        foo[8] * bar[8] + foo[9] * bar[9] +
+        foo[10] * bar[10] + foo[11] * bar[11] +
+        foo[12] * bar[12] + foo[13] * bar[13] +
+        foo[14] * bar[14] + foo[15] * bar[15] +
+        foo[16] * bar[16] + foo[17] * bar[17] +
+        foo[18] * bar[18] + foo[19] * bar[19];
+    }
+    timer.stop();
+
+    std::cout << "Native sum of 20 elements speed: "
+              << timer.getSeconds() << std::endl;
+
+
+
+
+    return 0;
+}
