7189530e9d659b2ca1555a484e6626c2dfe4a194
Aman Orazaev <AOrazaev@gmail.com>
2012-12-11 23:48:45 +0400

    edu/shad/babenko/hw6: +fixes +stress testing

--- edu/shad/babenko/code/hw6.p2.cpp
+++ edu/shad/babenko/code/hw6.p2.cpp
@@ -4,6 +4,8 @@
 
 #include <math.h>
 #include <cassert>
+#include <cstdlib>
+#include <cstdio>
 
 
 
@@ -34,6 +36,16 @@ class TAgent {
 };
 
 typedef std::vector<TAgent> TAgentsVector;
+
+
+
+
+
+
+std::ostream& operator<< (std::ostream& out, const TAgent& agent) {
+    out << "[" << agent.X() << ", " << agent.Y() << "]";
+}
+
 
 
 
@@ -81,7 +93,7 @@ class TEdge {
     }
 
     bool operator< (const TEdge& rhs) const {
-        // Use NOT for get minimum on top of std::priority_queue
+        // Use 'NOT' for get minimum on top of std::priority_queue
         return !(GetWeight() < rhs.GetWeight());
     }
 };
@@ -153,12 +165,12 @@ class TAgentsDSU {
     std::vector<TDSUElement> nodes;
 
     public:
-    explicit TAgentsDSU(const TAgentsVector& agents)
-        : numberOfClasses(agents.size())
+    explicit TAgentsDSU(size_t agentsSize)
+        : numberOfClasses(agentsSize)
         , nodes() {
-        nodes.reserve(agents.size());
+        nodes.reserve(agentsSize);
 
-        for (int i = 0; i < agents.size(); ++i) {
+        for (int i = 0; i < agentsSize; ++i) {
             nodes.push_back(TDSUElement(i));
         }
     }
@@ -206,7 +218,9 @@ TEdgeHeap CreateEdgeHeap(TAgentsVector* const agents) {
         for (size_t secondAgent = 0;
              secondAgent < agents->size();
              ++secondAgent) {
-            resultHeap.push(TEdge(agents, firstAgent, secondAgent));
+            if (firstAgent != secondAgent) {
+                resultHeap.push(TEdge(agents, firstAgent, secondAgent));
+            }
         }
     }
 
@@ -220,9 +234,13 @@ TEdgeHeap CreateEdgeHeap(TAgentsVector* const agents) {
 
 
 double FindRange(TAgentsVector* const agents) {
+    if (agents->size() < 2) {
+        return 0.0;
+    }
+
     TEdgeHeap edgeHeap = CreateEdgeHeap(agents);
 
-    TAgentsDSU dsu(*agents);
+    TAgentsDSU dsu(agents->size());
 
     TEdge currentEdge;
     while (dsu.GetNumberOfClasses() > 1) {
@@ -277,7 +295,7 @@ void test_TAgentsDSU() {
     agents.push_back(TAgent(0, 3));
     agents.push_back(TAgent(0, 9));
 
-    TAgentsDSU dsu(agents);
+    TAgentsDSU dsu(agents.size());
 
     assert(!dsu.Equivalent(0, 1));
     assert(dsu.Equivalent(0, 0));
@@ -300,9 +318,299 @@ void test_TAgentsDSU() {
 
 
 
+
+/////////////////////////// Stress testing here ///////////////////////////
+
+
+
+
+template <typename T>
+void printElements(T begin, T end) {
+    while (begin != end) {
+        std::cout << *begin++ << " ";
+    }
+    std::cout << std::endl;
+}
+
+
+
+
+
+class TAgentNode;
+
+typedef std::vector<TAgentNode> TAgentNodeVector;
+
+
+
+class TAgentNode : public TAgent {
+    std::vector<TAgentNode*> agentsInRange;
+
+    bool wasHere;
+
+    public:
+    TAgentNode(int x, int y)
+        : TAgent(x, y)
+        , agentsInRange()
+        , wasHere(false) {
+    }
+
+    void AddAgentInRange(TAgentNode* const pAgentNode) {
+        agentsInRange.push_back(pAgentNode);
+    }
+
+    std::vector<TAgentNode*>::iterator begin() {
+        return agentsInRange.begin();
+    }
+
+    std::vector<TAgentNode*>::iterator end() {
+        return agentsInRange.end();
+    }
+
+    std::vector<TAgentNode*>::const_iterator begin() const {
+        return agentsInRange.begin();
+    }
+
+    std::vector<TAgentNode*>::const_iterator end() const {
+        return agentsInRange.end();
+    }
+
+    size_t GetNumberOfConnectedAgents() const {
+        return agentsInRange.size();
+    }
+
+    bool WasHere() const {
+        return wasHere;
+    }
+
+    void SetWasHere(bool newWasHere) {
+        wasHere = newWasHere;
+    }
+
+    typedef std::vector<TAgentNode*>::iterator iterator;
+    typedef std::vector<TAgentNode*>::const_iterator const_iterator;
+};
+
+
+
+
+
+
+
+double CalculateRange(const TAgentNode& first, const TAgentNode& second) {
+    return sqrt(static_cast<double>(
+        pow(first.X() - second.X(), 2) +
+        pow(first.Y() - second.Y(), 2)));
+}
+
+
+
+
+
+
+
+TAgentNodeVector CreateAgentNodeVector(
+    const TAgentsVector& agents,
+    double range
+) {
+    TAgentNodeVector result;
+
+    for (TAgentsVector::const_iterator cit = agents.begin();
+         cit != agents.end();
+         ++cit) {
+        result.push_back(TAgentNode(cit->X(), cit->Y()));
+    }
+
+
+    for (size_t current = 0;
+         current < result.size();
+         ++current) {
+        for (size_t other = 0;
+             other < result.size();
+             ++other) {
+            if (other == current) {
+                continue;
+            }
+
+            if (CalculateRange(result[current], result[other]) <= range) {
+                result[current].AddAgentInRange(&result[other]);
+            }
+        }
+    }
+
+    return result;
+}
+
+
+
+
+
+std::ostream& operator<< (std::ostream& out, const TAgentNode& agent) {
+    out << "[(" << agent.X() << ", " << agent.Y() << "), (";
+
+    for (TAgentNode::const_iterator cit = agent.begin();
+         cit != agent.end();
+         ++cit) {
+        std::cout << "<" << (*cit)->X() << ", " << (*cit)->Y() << ">";
+        if (cit + 1 != agent.end()) {
+            std::cout << " ";
+        }
+    }
+    std::cout << ")]";
+}
+
+
+
+
+
+size_t NumberOfConnectedNodes(
+    TAgentNode* const agent
+) {
+    size_t connected = 0;
+    if (agent->WasHere()) {
+        return connected;
+    }
+
+    agent->SetWasHere(true);
+    ++connected;
+
+    for (TAgentNode::const_iterator cit = agent->begin();
+         cit != agent->end();
+         ++cit) {
+        if (!(*cit)->WasHere()) {
+            connected += NumberOfConnectedNodes(*cit);
+        }
+    }
+
+    return connected;
+}
+
+
+
+
+
+
+bool IsAssociatedGraph(TAgentNodeVector& graph) {
+    if (graph.size() == 0) {
+        return true;
+    }
+
+    return NumberOfConnectedNodes(&graph[0]) == graph.size();
+}
+
+
+
+
+
+
+TAgent CreateRandomAgent() {
+    int x = (rand() % 1000000) * pow(-1, rand() % 2);
+    int y = (rand() % 1000000) * pow(-1, rand() % 2);
+    return TAgent(x, y);
+}
+
+
+
+
+
+
+TAgentsVector CreateRandomAgentsVector(
+    size_t maxSize,
+    size_t minSize = 10) {
+    size_t size = rand() % (maxSize - minSize) + minSize;
+
+    TAgentsVector agents;
+
+    for (int i = 0; i < size; ++i) {
+        agents.push_back(CreateRandomAgent());
+    }
+
+    return agents;
+}
+
+
+
+
+
+class TTimer {
+    clock_t startClocks;
+    clock_t endClocks;
+
+    public:
+    TTimer()
+        : startClocks(0)
+        , endClocks(0) {
+    }
+
+    void Start() {
+        startClocks = clock();
+    }
+
+    void Stop() {
+        endClocks = clock();
+    }
+
+    double GetSeconds() const {
+        return static_cast<double>(
+            endClocks - startClocks) / CLOCKS_PER_SEC;
+    }
+
+    clock_t GetClocks() const {
+        return endClocks - startClocks;
+    }
+};
+
+
+
+
+
+
+
+const double accuracy = 0.00000001;
+
+void test_Stress(size_t numberOfTests) {
+    TTimer timer;
+    for (size_t n = 0; n < numberOfTests; ++n) {
+        std::cout << "Test #" << n + 1 << std::endl;
+
+        timer.Start();
+        TAgentsVector agents = CreateRandomAgentsVector(1001, 1000);
+        // std::cout << "    Agents: ";
+        // printElements(agents.begin(), agents.end());
+        std::cout << "    Agents size: " << agents.size() << std::endl;
+
+        double ans = FindRange(&agents);
+        timer.Stop();
+        std::cout << "    Founded range: ";
+        printf("%.10f\n", ans);
+        std::cout << "    TIME: " << timer.GetSeconds() << std::endl;
+        assert(timer.GetSeconds() < 2.5);
+
+        TAgentNodeVector associated =
+                    CreateAgentNodeVector(agents, ans);
+
+        // std::cout << "    Associated: ";
+        // printElements(associated.begin(), associated.end());
+
+        assert(IsAssociatedGraph(associated));
+
+        if (ans - accuracy > 0) {
+            TAgentNodeVector notAssociated =
+                        CreateAgentNodeVector(agents, ans - accuracy);
+            assert(!IsAssociatedGraph(notAssociated));
+        }
+    }
+}
+
+
+
+
+
+
 int main() {
+    std::srand(360);
     test_TEdge();
     test_TAgentsDSU();
+    test_Stress(100);
 
     return 0;
 }
