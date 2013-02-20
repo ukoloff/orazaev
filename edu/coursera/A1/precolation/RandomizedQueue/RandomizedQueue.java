8fb84e1ef1a9742ee5e6eae59349053db08fa7f9
aorazaev <aorazaev@gmail.com>
2013-02-21 03:26:51 +0400

    A1: add first randomizedQueue implementation, need testing.

---
+++ edu/coursera/A1/precolation/RandomizedQueue/RandomizedQueue.java
@@ -0,0 +1,120 @@
+import java.util.Iterator;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+    private Item[] data;
+    private int sz;
+    private int capacity;
+
+    public RandomizedQueue() {
+        this.sz = 0;
+        this.capacity = 1;
+        data = (Item[]) new Object[1];
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public int size() {
+        return sz;
+    }
+
+    public void enqueue(Item item) {
+        if (item == null) {
+            throw new java.lang.NullPointerException();
+        }
+
+        if (size() == capacity) {
+            increaseCapacity();
+        }
+
+        data[sz++] = item;
+        swapLastWithAnotherUniformly();
+    }
+
+    public Item dequeue() {
+        checkEmpty();
+        swapLastWithAnotherUniformly();
+        Item result = data[--sz];
+        
+        if (size() * 4 == capacity) {
+            decreaseCapacity();
+        }
+
+        return result;
+    }
+
+    public Item sample() {
+        checkEmpty();
+        swapLastWithAnotherUniformly();
+        return data[size() - 1];
+    }
+
+    public Iterator<Item> iterator() {
+        return new RandomizedQueueIterator();
+    }
+
+    public static void main(String[] args) {
+        RandomizedQueue<Integer> rq = new RandomizedQueue<Integer>();
+    }
+
+    private void increaseCapacity() {
+        Item[] oldData = data;
+        Item[] newData = java.util.Arrays.copyOf(data, data.length * 2);
+
+        data = newData;
+        oldData = null;
+    }
+
+    private void decreaseCapacity() {
+        Item[] oldData = data;
+        Item[] newData = java.util.Arrays.copyOf(data, data.length / 2);
+
+        data = newData;
+        oldData = null;
+    }
+
+    private void swapLastWithAnotherUniformly() {
+        swapItems(StdRandom.uniform(size()), size() - 1);
+    }
+
+    private void swapItems(int firstIndex, int secondIndex) {
+        Item temp = data[firstIndex];
+        data[firstIndex] = data[secondIndex];
+        data[secondIndex] = temp;
+        temp = null;
+    }
+
+    private void checkEmpty() {
+        if (isEmpty()) {
+            throw new java.util.NoSuchElementException();
+        }
+    }
+
+    private class RandomizedQueueIterator implements Iterator<Item> {
+        int current;
+        int[] order;
+
+        public RandomizedQueueIterator() {
+            this.current = 0;
+            this.order = new int[size()];
+            StdRandom.shuffle(order);
+        }
+
+        public boolean hasNext() {
+            return current != size();
+        }
+
+        public Item next() {
+            if (!hasNext()) {
+                throw new java.util.NoSuchElementException();
+            }
+
+            return data[order[current++]];
+        }
+
+        public void remove() {
+            throw new java.lang.UnsupportedOperationException();
+        }
+    }
+}
