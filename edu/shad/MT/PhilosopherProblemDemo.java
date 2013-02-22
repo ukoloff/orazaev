c5007b21f23d0ebae24517be4c2122e204566896
Aman Orazaev <AOrazaev@gmail.com>
2013-02-22 17:51:30 +0400

    shad/MT: add polite mode for phils, add stddev and mean calculation.

--- edu/shad/MT/PhilosopherProblemDemo.java
+++ edu/shad/MT/PhilosopherProblemDemo.java
@@ -22,13 +22,17 @@ class Fork implements Comparable<Fork> {
 
 class Philosopher {
     private int eatCount;
-    private long waitTime;
+    private volatile long waitTime;
     private long startWait;
-    private Random random;
 
+    protected Random random;
     protected int position;
     protected Fork left;
     protected Fork right;
+
+    protected boolean verbose;
+
+    protected final int MAX_TIME_TO_WAIT = 100;
 
     public Philosopher(int position, Fork left, Fork right, int seed) {
         if (left == right) {
@@ -44,6 +48,8 @@ class Philosopher {
         this.position = position;
         this.left = left;
         this.right = right;
+
+        this.verbose = true;
     }
 
     public Philosopher(int position, Fork left, Fork right) {
@@ -54,18 +60,22 @@ class Philosopher {
     public void startEating() {
         waitTime += System.currentTimeMillis() - startWait;
 
-        System.out.println("[Philosopher " + position + "] is eating.");
-        tryToSleep(random.nextInt(100));
+        if (verbose)
+            System.out.println("[Philosopher " + position + "] is eating.");
+        tryToSleep(random.nextInt(MAX_TIME_TO_WAIT));
 
         ++eatCount;
-        System.out.println("[Philosopher " + position + "] finished eating.");
+        if (verbose)
+            System.out.println("[Philosopher " + position + "] finished eating.");
     }
 
     public void startThinking() {
-        System.out.println("[Philosopher " + position + "] is thinking.");
-        tryToSleep(random.nextInt(100));
+        if (verbose)
+            System.out.println("[Philosopher " + position + "] is thinking.");
+        tryToSleep(random.nextInt(MAX_TIME_TO_WAIT));
 
-        System.out.println("[Philosopher " + position + "] is hungry.");
+        if (verbose)
+            System.out.println("[Philosopher " + position + "] is hungry.");
         startWait = System.currentTimeMillis();
     }
 
@@ -81,7 +91,7 @@ class Philosopher {
         return eatCount;
     }
 
-    private void tryToSleep(int mseconds) {
+    protected void tryToSleep(int mseconds) {
         try {
             Thread.sleep(mseconds);
         } catch (InterruptedException e) {
@@ -96,15 +106,48 @@ class Philosopher {
 
 class RunnablePhilosopher extends Philosopher implements Runnable {
     private volatile boolean stopFlag;
+    private boolean politeMode;
+    private RunnablePhilosopher leftNeighbour;
+    private RunnablePhilosopher rightNeighbour;
 
     RunnablePhilosopher(int position, Fork left, Fork right, int seed) {
         super(position, left, right, seed);
+        this.politeMode = false;
         this.stopFlag = false;
+        this.leftNeighbour = null;
+        this.rightNeighbour = null;
     }
 
     RunnablePhilosopher(int position, Fork left, Fork right) {
-        super(position, left, right);
-        this.stopFlag = false;
+        this(position, left, right, 0);
+    }
+
+    public void setLeftNeighbour(RunnablePhilosopher leftNeighbour) {
+        this.leftNeighbour = leftNeighbour;
+    }
+
+    public void setRightNeighbour(RunnablePhilosopher rightNeighbour) {
+        this.rightNeighbour = rightNeighbour;
+    }
+
+    public RunnablePhilosopher turnOnPoliteMode() {
+        this.politeMode = true;
+        return this;
+    }
+
+    public RunnablePhilosopher verboseOff() {
+        this.verbose = false;
+        return this;
+    }
+
+    private boolean bePolite(RunnablePhilosopher other) {
+        if (other != null
+            && other.getWaitTime() - getWaitTime() > MAX_TIME_TO_WAIT * 2) {
+            tryToSleep(random.nextInt(MAX_TIME_TO_WAIT));
+            return true;
+        }
+
+        return false;
     }
 
     public void run() {
@@ -113,18 +156,25 @@ class RunnablePhilosopher extends Philosopher implements Runnable {
             Fork first = (left.compareTo(right) < 0) ? left : right;
             Fork second = (left.compareTo(right) < 0) ? right : left;
 
+            if (politeMode && !bePolite(leftNeighbour)) {
+                bePolite(rightNeighbour);
+            }
+
             synchronized (first) {
-                    System.out.println("[Philosopher " + position
-                            + "] took first fork.");
+                    if (verbose)
+                        System.out.println("[Philosopher " + position
+                                + "] took first fork.");
                 synchronized (second) {
-                    System.out.println("[Philosopher " + position
-                            + "] took second fork.");
+                    if (verbose)
+                        System.out.println("[Philosopher " + position
+                                + "] took second fork.");
                     startEating();
                 }
             }
         }
 
-        System.out.println("[Philosopher " + position + "] stopped.");
+        if (verbose)
+            System.out.println("[Philosopher " + position + "] stopped.");
     }
 
     public void stop() {
@@ -137,22 +187,53 @@ class RunnablePhilosopher extends Philosopher implements Runnable {
 
 
 public class PhilosopherProblemDemo {
-    public static void main(String[] args)
-    throws java.lang.InterruptedException {
-        int count = 5;
-        int seed = 360;
-        int dinnerTimeInSeconds = 60;
+    private int count;
+    private int dinnerTimeInSeconds;
+    private int seed;
 
+    public PhilosopherProblemDemo(int count, int dinnerTimeInSeconds, int seed) {
+        this.count = count;
+        this.dinnerTimeInSeconds = dinnerTimeInSeconds;
+        this.seed = seed;
+    }
+
+    public void Demo() {
+        RunnablePhilosopher[] phils = createPhilosophers();
+
+        try {
+            dinner(phils);
+        } catch (java.lang.InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        printSummary(phils);
+    }
+
+    private RunnablePhilosopher[] createPhilosophers() {
         RunnablePhilosopher[] phils = new RunnablePhilosopher[count];
 
         Fork last = new Fork();
         Fork left = last;
         for (int i = 0; i < count; ++i) {
             Fork right = (i == count - 1) ? last : new Fork();
-            phils[i] = new RunnablePhilosopher(i, left, right, seed + i);
+            phils[i] = new RunnablePhilosopher(i, left, right, seed + i)
+                    .turnOnPoliteMode()
+                    .verboseOff();
             left = right;
         }
 
+        for (int i = 0; i < count; ++i) {
+            phils[i].setRightNeighbour(
+                    (i != count - 1) ? phils[i + 1] : phils[0]);
+            phils[i].setLeftNeighbour(
+                    (i != 0) ? phils[i - 1] : phils[count - 1]);
+        }
+
+        return phils;
+    }
+
+    private void dinner(RunnablePhilosopher[] phils)
+    throws java.lang.InterruptedException {
         Thread[] threads = new Thread[count];
 
         for (int i = 0; i < count; ++i) {
@@ -169,11 +250,45 @@ public class PhilosopherProblemDemo {
         for (Thread thread : threads) {
             thread.join();
         }
+    }
 
+    private void printSummary(RunnablePhilosopher[] phils) {
         for (RunnablePhilosopher phil : phils) {
             System.out.println("Philosopher " + phil.getPosition()
                     + " ate = " + phil.getEatCount()
                     + " times and waited = " + phil.getWaitTime() + " ms.");
         }
+
+        System.out.println("-------------- Summary ---------------");
+        System.out.println("Mean: " + calculateMean(phils) + " ms.");
+        System.out.println("Stddev: " + calculateStddev(phils) + ".");
+    }
+
+    private double calculateMean(Philosopher[] phils) {
+        double mean = 0;
+        for (Philosopher phil : phils) {
+            mean += phil.getWaitTime() / phils.length;
+        }
+
+        return mean;
+    }
+
+    private double calculateStddev(Philosopher[] phils) {
+        if (phils.length == 1) {
+            return Double.NaN;
+        }
+
+        double mean = calculateMean(phils);
+        double stddev = 0;
+        for (Philosopher phil : phils) {
+            stddev += (phil.getWaitTime() - mean) * (phil.getWaitTime() - mean)
+                    / (phils.length - 1);
+        }
+
+        return Math.sqrt(stddev);
+    }
+
+    public static void main(String[] args) {
+        new PhilosopherProblemDemo(5, 10, 360).Demo();
     }
 }
