8d8438e9baff0c4fc730798999335b7a670a2a5c
aorazaev <aorazaev@gmail.com>
2013-03-13 04:33:47 +0400

    shad/MT: move messages logic to messages.{h,cpp}

---
+++ edu/shad/MT/hw1.p2/src/messages.cpp
@@ -0,0 +1,56 @@
+#include <messages.h>
+
+void TGetMessageHandler::Process(const TTaskMessage& msg) {
+    printf("GET: %s\n", msg.GetData()->c_str());
+    char buffer[500];
+
+    sprintf(buffer, "Loggin get message = '%s'", msg.GetData()->c_str());
+    TTaskMessage logTask(buffer, T_LOG);
+    env_.taskQueue->Put(logTask);
+
+    sprintf(buffer, "Parsing message = '%s'", msg.GetData()->c_str());
+    TTaskMessage parseTask(buffer, T_PARSE);
+    env_.taskQueue->Put(parseTask);
+}
+
+void TParseMessageHandler::Process(const TTaskMessage& msg) {
+    printf("PARSE: %s\n", msg.GetData()->c_str());
+    char buffer[500];
+
+    sprintf(buffer, "Loggin parse message = '%s'", msg.GetData()->c_str());
+    TTaskMessage logTask(buffer, T_LOG);
+    env_.taskQueue->Put(logTask);
+}
+
+void TPoisonMessageHandler::Process(const TTaskMessage&) {
+    printf("POISON:\n");
+
+    TTaskMessage poisonedTask("", T_POISON);
+    env_.taskQueue->Put(poisonedTask);
+    env_.alive = false;
+}
+
+void TLogMessageHandler::Process(const TTaskMessage& msg) {
+    printf("LOG: %s\n", msg.GetData()->c_str());
+}
+
+void TMsgProcessor::Process(
+        const TTaskMessage& msg,
+        TWorkerEnvironment& env)
+{
+    switch(msg.GetType()) {
+        case T_GET:
+            TGetMessageHandler(env).Process(msg);
+            break;
+        case T_PARSE:
+            TParseMessageHandler(env).Process(msg);
+            break;
+        case T_POISON:
+            TPoisonMessageHandler(env).Process(msg);
+            break;
+        case T_LOG:
+            TLogMessageHandler(env).Process(msg);
+            break;
+    };
+}
+
--- edu/shad/MT/hw1.p2/src/messages.h
+++ edu/shad/MT/hw1.p2/src/messages.h
@@ -1,5 +1,5 @@
 /**
-    @brief Thread messages description.
+    @brief Thread messages description, environment and message handlers.
 
     Need for thread message queue. Each message
     type should have its handler or processor.
@@ -20,12 +20,30 @@
         queue and one individual thread to dump pages.
 
 
+    Each worker environment has common message
+    queue with tasks (GET, PARSE, POISON), log
+    messages queue for dumping pages and 
+    common set for check twice downloading.
+
+
+    Also has TMsgProcessor class. Thread should call:
+        TMsgProcessor::Process(msg, env);
+    to process msg. TMsgProcessor will call message
+    type handler.
+
+    If you add new message type, you need define
+    it's handler class also and add it to TMsgProcessor.
+
+
     @author Aman Orazaev
 */
 #pragma once
 
 #include <memory>
 #include <string>
+
+#include <queue.h>
+#include <set.h>
 
 /** @brief Message type for TTaskMessage. */
 enum TMessageType {T_GET, T_PARSE, T_POISON, T_LOG};
@@ -72,3 +90,112 @@ private:
 
 
 
+typedef std::shared_ptr<TSynchronizedQueue<TTaskMessage> >
+        TMsgQueueHolder;
+
+typedef std::shared_ptr<TSynchronizedSet<std::string> >
+        TStringSetHolder;
+
+
+/**
+    @brief worker thread environment.
+
+    TODO: add downloader, parser, and handler factory.
+*/
+struct TWorkerEnvironment {
+    TMsgQueueHolder taskQueue;
+    TMsgQueueHolder logQueue;
+    TStringSetHolder downloadedUrls;
+
+    /** alive = true, until wasn't processed poisoned message. */
+    bool alive;
+
+    /// FIXME(orazaev@): add downloader and parser here.
+    /// FIXME(orazaev@): add message handlers factory.
+
+    TWorkerEnvironment()
+        : taskQueue(new TSynchronizedQueue<TTaskMessage>())
+        , logQueue(new TSynchronizedQueue<TTaskMessage>())
+        , downloadedUrls(new TSynchronizedSet<std::string>())
+        , alive(true)
+    { }
+};
+
+
+
+/** @brief Abstract message handler. */
+class TMessageHandler {
+protected:
+    TMessageHandler(TWorkerEnvironment& env)
+        : env_(env)
+    { }
+    ~TMessageHandler() { }
+
+protected:
+    void Process(TTaskMessage&);
+
+protected:
+    TWorkerEnvironment& env_;
+};
+
+
+
+/** @brief T_GET message handler. */
+class TGetMessageHandler : public TMessageHandler {
+public:
+    TGetMessageHandler(TWorkerEnvironment& env)
+        : TMessageHandler(env)
+    { }
+
+    void Process(const TTaskMessage& msg);
+};
+
+
+
+/** @brief T_PARSE message handler. */
+class TParseMessageHandler : public TMessageHandler {
+public:
+    TParseMessageHandler(TWorkerEnvironment& env)
+        : TMessageHandler(env)
+    { }
+
+    void Process(const TTaskMessage& msg);
+};
+
+
+
+/** @brief T_POISON message handler. */
+class TPoisonMessageHandler : public TMessageHandler {
+public:
+    TPoisonMessageHandler(TWorkerEnvironment& env)
+        : TMessageHandler(env)
+    { }
+
+    void Process(const TTaskMessage&);
+};
+
+
+
+/** @brief T_LOG message handler. */
+class TLogMessageHandler : public TMessageHandler {
+public:
+    TLogMessageHandler(TWorkerEnvironment& env)
+        : TMessageHandler(env)
+    { }
+
+    void Process(const TTaskMessage& msg);
+};
+
+
+
+/** @brief Message handlers trigger. */
+class TMsgProcessor {
+public:
+    static void Process(
+            const TTaskMessage& msg,
+            TWorkerEnvironment& env);
+
+private:
+    TMsgProcessor() = delete;
+    ~TMsgProcessor() = delete;
+};
