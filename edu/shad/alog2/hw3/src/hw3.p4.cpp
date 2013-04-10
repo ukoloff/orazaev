39261849092b307437b2080f007f28ad51c399a9
Aman Orazaev <AOrazaev@gmail.com>
2013-04-11 00:36:05 +0400

    shad/algo2: developing suffix tree

--- edu/shad/alog2/hw3/src/hw3.p4.cpp
+++ edu/shad/alog2/hw3/src/hw3.p4.cpp
@@ -3,48 +3,70 @@
 #include <cassert>
 #include <unordered_map>
 
-class TNode {
+class TSuffixTree {
 public:
-    TNode()
-        : suffixLink(0)
-        , edgeLabelBegin(0)
-        , edgeLabelEnd(0)
-        , depth(0)
+    TSuffixTree()
+        : text("")
+        , nodes(1, TNode())
     { }
 
-    TNode(size_t edgeLabelBegin, size_t edgeLabelEnd, size_t depth)
-        : suffixLink(0)
-        , edgeLabelBegin(edgeLabelBegin)
-        , edgeLabelEnd(edgeLabelEnd)
-        , depth(depth)
+    TSuffixTree(const std::string& text)
+        : text(text)
+        , nodes(1, TNode())
     { }
-
-    inline size_t GetSuffixLink() const { return suffixLink; }
-    inline size_t GetEdgeLabelBegin() const { return edgeLabelBegin; }
-    inline size_t GetEdgeLabelEnd() const { return edgeLabelEnd; }
-    inline size_t GetDepth() const { return depth; }
-
-    inline void SetSuffixLink(size_t link) { suffixLink = link; }
-
-    /**
-        @brief Get child node index in nodes vector.
-        @return child node index if has and TNode::NONE otherwise.
-    */
-    size_t GetChild(char edgeFirstChar) const;
-    static const size_t NONE = static_cast<size_t>(-1);
-
-    void AddChild(char edgeFirstChar, size_t childIndex);
 
 private:
-    size_t suffixLink;
-    size_t edgeLabelBegin;
-    size_t edgeLabelEnd;
-    size_t depth;
-    std::unordered_map<char, size_t> children;
+    /**
+        @brief Node class for suffix tree.
+    */
+    class TNode {
+    public:
+        TNode()
+            : suffixLink(0)
+            , edgeLabelBegin(0)
+            , edgeLabelEnd(0)
+            , depth(0)
+        { }
+
+        TNode(size_t edgeLabelBegin, size_t edgeLabelEnd, size_t depth)
+            : suffixLink(0)
+            , edgeLabelBegin(edgeLabelBegin)
+            , edgeLabelEnd(edgeLabelEnd)
+            , depth(depth)
+        { }
+
+        inline size_t GetSuffixLink() const { return suffixLink; }
+        inline size_t GetEdgeLabelBegin() const { return edgeLabelBegin; }
+        inline size_t GetEdgeLabelEnd() const { return edgeLabelEnd; }
+        inline size_t GetDepth() const { return depth; }
+
+        inline void SetSuffixLink(size_t link) { suffixLink = link; }
+        inline void SetEdgeLabelEnd(size_t newEnd) { edgeLabelEnd = newEnd; }
+
+        /**
+            @brief Get child node index in nodes vector.
+            @return child node index if has and TNode::NONE otherwise.
+        */
+        size_t GetChild(char edgeFirstChar) const;
+        static const size_t NONE = static_cast<size_t>(-1);
+
+        void AddChild(char edgeFirstChar, size_t childIndex);
+
+    private:
+        size_t suffixLink;
+        size_t edgeLabelBegin;
+        size_t edgeLabelEnd;
+        size_t depth;
+        std::unordered_map<char, size_t> children;
+    };
+
+private:
+    std::string text;
+    std::vector<TNode> nodes;
 };
 
 
-size_t TNode::GetChild(char edgeFirstChar) const {
+size_t TSuffixTree::TNode::GetChild(char edgeFirstChar) const {
     auto iter = children.find(edgeFirstChar);
     if (iter == children.end()) {
         return TNode::NONE;
@@ -53,7 +75,7 @@ size_t TNode::GetChild(char edgeFirstChar) const {
     return iter->second;
 }
 
-void TNode::AddChild(char edgeFirstChar, size_t childIndex) {
+void TSuffixTree::TNode::AddChild(char edgeFirstChar, size_t childIndex) {
     assert(children.find(edgeFirstChar) == children.end());
     children[edgeFirstChar] = childIndex;
 }
