72f62f003954e10ed4c02efe3f987e4bcf8bfd0e
orazaev <orazaev@orion.maps.dev.yandex.net>
2013-10-11 18:04:10 +0400

    Changes in host_config.sh

--- config/host_configure.sh
+++ config/host_configure.sh
@@ -3,3 +3,13 @@
 # vim configure
 ln -s $PWD/.vimrc ~/.vimrc
 ln -s $PWD/.vim ~/.vim
+
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
+# python configure
+ln -s $PWD/.pythonstartup ~/.pythonstartup
+
+
+echo "export EDITOR=vim" >> ${HOME}/.bashrc
+echo "export PYTHONSTARTUP=~/.pythonstartup" >> ${HOME}/.bashrc
+echo "export TERM=xterm-256color" >> ${HOME}/.bashrc
